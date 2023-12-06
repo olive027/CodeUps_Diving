16de9085fd2a3472853feda5fc487f498402c0b5
olive027 <blueblueww@yahoo.co.jp>
2023-12-06 14:31:26 +0900

    231206

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,26 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
+	// hamburger
+$('.js-hamburger').click(function(){
+	$('.js-hamburger').toggleClass('is-active');
+	$('.js-sp-nav').fadeToggle(600);
+	$('html').toggleClass('is-fixed');//ハンバーガー展開時に背景を固定
+})
+
+//mv swiper
+// swiper
+const swiper = new Swiper(".swiper", {
+	loop: true,
+	autoplay: {
+		delay: 3000,
+		disableOnInteraction: false,
+	},
+	effect: 'fade',
+	speed: 2500,
+	allowTouchMove: false,
+});
+
+
 
 });
