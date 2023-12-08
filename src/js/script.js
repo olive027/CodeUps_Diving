895efc8e754198ea6c44cebaca7567353766cb86
olive027 <blueblueww@yahoo.co.jp>
2023-12-08 23:17:45 +0900

    231208

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,14 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
-//loading
+//============================ hamburger================================
+$('.js-hamburger').click(function(){
+	$('.js-hamburger').toggleClass('is-active');
+	$('.js-sp-nav').fadeToggle(600);
+	$('html').toggleClass('is-fixed');//ハンバーガー展開時に背景を固定
+});
+
+//============================loading==================================
 // var loadingTitle = document.querySelectorAll('.loading__title--white');　//要素のテキストを取得
 // loadingTitle.forEach((target) => {
 // 	// タグ内のテキストを一文字ずつ分割
@@ -42,15 +49,7 @@ tl.eventCallback('onComplete', function () {
   initSwiper();
 });
 
-
-	// hamburger
-$('.js-hamburger').click(function(){
-	$('.js-hamburger').toggleClass('is-active');
-	$('.js-sp-nav').fadeToggle(600);
-	$('html').toggleClass('is-fixed');//ハンバーガー展開時に背景を固定
-});
-
-//mv swiper
+//===============================mv swiper================================
 // swiper
 function initSwiper(){
 var swiper = new Swiper(".js-mv-swiper", {
@@ -65,7 +64,7 @@ var swiper = new Swiper(".js-mv-swiper", {
 });
 };
 
-// campaign swiper
+//====================== campaign swiper===============================
 var swiper = new Swiper(".js-campaign-swiper", {
 	loop: true,
 	autoplay: {
@@ -88,6 +87,33 @@ var swiper = new Swiper(".js-campaign-swiper", {
     nextEl: ".js-campaign-btn-next",
     prevEl: ".js-campaign-btn-prev",
   },
+});
+
+// ==================== 画像の出現アニメーション ===========================
+//要素の取得とスピードの設定
+var box = $('.js-colorbox'),
+    speed = 700;
+
+//.colorboxの付いた全ての要素に対して下記の処理を行う
+box.each(function(){
+    $(this).append('<div class="color"></div>')
+    var color = $(this).find($('.color'));
+    var image = $(this).find('img');
+    var counter = 0;
+
+    image.css('opacity','0');
+    color.css('width','0%');
+    //inviewを使って背景色が画面に現れたら処理をする
+    color.on('inview', function(){
+        if(counter == 0){
+					$(this).delay(200).animate({'width':'100%'},speed,function(){
+                   image.css('opacity','1');
+                   $(this).css({'left':'0' , 'right':'auto'});
+                   $(this).animate({'width':'0%'},speed);
+                })
+            counter = 1;
+          }
+     });
 });
 
 
